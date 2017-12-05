8c6256f266d70489d52bb83d187fce399e436804
Carlos Sanchez <papachoco@gmail.com>
2017-12-05 16:51:35 -0600

    pep8; clean imports

--- src/nti/app/analytics_pandas/views/__init__.py
+++ src/nti/app/analytics_pandas/views/__init__.py
@@ -4,35 +4,32 @@
 .. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import, division
-__docformat__ = "restructuredtext en"
-
-logger = __import__('logging').getLogger(__name__)
-
-from .. import MessageFactory
-
-from zope import interface
-from zope import component
- 
-from zope.container.contained import Contained
- 
-from zope.location.interfaces import IContained
- 
-from zope.traversing.interfaces import IPathAdapter
-
-from nti.dataserver.interfaces import IDataserverFolder
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
 
 from pyramid.interfaces import IRequest
 
+from zope import interface
+from zope import component
+
+from zope.container.contained import Contained
+
+from zope.location.interfaces import IContained
+
 from zope.traversing.interfaces import IPathAdapter
- 
- 
+
+from nti.app.analytics_pandas import MessageFactory
+
+from nti.dataserver.interfaces import IDataserverFolder
+
+
 @interface.implementer(IPathAdapter)
 @component.adapter(IDataserverFolder, IRequest)
 class PandasReportAdapter(Contained):
- 
+
     __name__ = 'pandas_reports'
- 
+
     def __init__(self, context, request):
         self.context = context
         self.request = request
