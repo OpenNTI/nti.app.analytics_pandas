5347327ff4620dbf8d0f8b50f3535370eb52f525
Egawati Panjei <egawati.panjei@gmail.com>
2018-03-24 01:59:08 +0700

    test resource views

--- src/nti/app/analytics_pandas/views/tests/test_resource_views.py
+++ src/nti/app/analytics_pandas/views/tests/test_resource_views.py
@@ -1,92 +1,73 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import print_function, unicode_literals, absolute_import, division
-__docformat__ = "restructuredtext en"
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
 
-# disable: accessing protected members, too many methods
-# pylint: disable=W0212,R0904
+# pylint: disable=protected-access,too-many-public-methods
 
 from hamcrest import is_
-from hamcrest import has_length
+from hamcrest import none
+from hamcrest import is_not
 from hamcrest import assert_that
-from hamcrest import greater_than
+from hamcrest import has_entries
 from hamcrest import has_property
 
-import os
-import json
+from pyramid.testing import DummyRequest
 
-from zope.cachedescriptors.property import Lazy
-
-from z3c.rml import rml2pdf
-
-from nti.app.analytics_pandas.views.commons import cleanup_temporary_file
-
-from nti.app.analytics_pandas.views.resource_views import ResourceViewsTimeseriesReportView
-from nti.app.analytics_pandas.views.resource_views import ResourceViewsTimeseriesContext
-
-from nti.app.analytics_pandas.tests import AppAnalyticsTestBase
 from nti.app.analytics_pandas.tests import PandasReportsLayerTest
 
-from nti.app.analytics_pandas.views.tests import _build_sample_context
+from nti.app.analytics_pandas.views.resource_views import ResourceViewsTimeseriesReportView
 
-from nti.app.pyramid_zope.z3c_zpt import ViewPageTemplateFile
+from nti.app.testing.application_webtest import ApplicationLayerTest
 
 from nti.app.testing.decorators import WithSharedApplicationMockDS
 
-from nti.externalization.externalization import to_external_object
 
-class TestResourceViews(AppAnalyticsTestBase):
+class TestResourceViewsView(ApplicationLayerTest):
 
-	def setUp(self):
-		super(TestResourceViews, self).setUp()
+    layer = PandasReportsLayerTest
 
-	@Lazy
-	def std_report_layout_rml(self):
-		path = os.path.join(os.path.dirname(__file__), '../../templates/std_report_layout.rml')
-		return path
+    @WithSharedApplicationMockDS(testapp=True, users=True)
+    def test_resource_views_report(self):
+        response = self.testapp.post_json('/dataserver2/pandas_reports/ResourceViewsReport',
+                                          {
+                                              'ntiid': 'tag:nextthought.com,2011-10:NTI-CourseInfo-Spring2015_SOC_1113',
+                                              'start_date': '2015-03-01',
+                                              'end_date': '2015-03-31'
+                                          },
+                                          extra_environ=self._make_extra_environ())
+        assert_that(response,
+                    has_property('content_type', 'application/pdf'))
 
-	def template(self, path):
-		result = ViewPageTemplateFile(path,
-									  auto_reload=(False,),
-									  debug=False)
-		return result
 
-	def test_std_report_layout_rml(self):
-		# make sure  template exists
-		path = self.std_report_layout_rml
-		assert_that(os.path.exists(path), is_(True))
+class TestResourceViewsOptions(ApplicationLayerTest):
 
-		# prepare view and context
-		start_date = '2015-01-01'
-		end_date = '2015-05-31'
-		courses = ['388']
-		period_breaks = '1 week'
-		minor_period_breaks = '1 day'
-		theme_bw_ = True
-		context = ResourceViewsTimeseriesContext(start_date=start_date, 
-						  end_date=end_date, 
-						  courses=courses,
-						  period_breaks=period_breaks, 
-						  minor_period_breaks=minor_period_breaks, 
-						  theme_bw_=theme_bw_)
-		
-		view = ResourceViewsTimeseriesReportView(context)
-		view._build_data('Bleach')
-		system = {'view':view, 'context':context}
-		rml = self.template(path).bind(view)(**system)
+    layer = PandasReportsLayerTest
 
-		pdf_stream = rml2pdf.parseString(rml)
-		result = pdf_stream.read()
-		assert_that(result, has_length(greater_than(1)))
-
-class TestReourceViewsView(PandasReportsLayerTest):
-	
-	@WithSharedApplicationMockDS(testapp=True, users=True)
-	def test_resource_views_view(self):
-		context = _build_sample_context(ResourceViewsTimeseriesContext)
-		params = to_external_object(context)
-		response = self.testapp.post('/dataserver2/pandas_reports/ResourceViews',
-                                    json.dumps(params),
-                                    extra_environ=self._make_extra_environ())
-		assert_that(response, has_property('content_type', 'application/pdf'))
+    @WithSharedApplicationMockDS(testapp=True, users=True)
+    def test_resource_views_report(self):
+        request = DummyRequest(params={'ntiid': 'tag:nextthought.com,2011-10:NTI-CourseInfo-Spring2015_SOC_1113',
+                                       'start_date': '2015-03-01',
+                                       'end_date': '2015-03-31'})
+        view = ResourceViewsTimeseriesReportView(request=request)
+        options = view()
+        assert_that(options, is_not(none()))
+        assert_that(options,
+                    has_entries('course_ids', is_([388]),
+                                'has_resource_view_events', True,
+                                'has_resource_views_per_enrollment_types', True,
+                                'has_resource_views_per_device_types', True,
+                                'has_resource_views_per_resource_types', True))
+        assert_that(options,
+                    has_entries('data',
+                                has_entries('resources_viewed', is_not(none()),
+                                            'resources_viewed', 
+                                            has_entries('num_rows', 30,
+                                                        'events_chart', is_not(none()),
+                                                        'tuples', (),
+                                                        'column_name', u'Resource Viewed')
+                                            )
+                                )
+                    )
