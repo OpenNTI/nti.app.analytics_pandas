e14d220300ca330dc597a65b595506fdc2c442c7
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-03-23 10:29:04 -0500

    try this

--- src/nti/app/analytics_pandas/views/tests/test_assessments.py
+++ src/nti/app/analytics_pandas/views/tests/test_assessments.py
@@ -1,81 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import print_function, unicode_literals, absolute_import, division
-__docformat__ = "restructuredtext en"
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
 
-# disable: accessing protected members, too many methods
-# pylint: disable=W0212,R0904
+# pylint: disable=protected-access,too-many-public-methods
 
-from hamcrest import is_
-from hamcrest import has_length
 from hamcrest import assert_that
-from hamcrest import greater_than
 from hamcrest import has_property
 
-import os
-import json
-
-from zope.cachedescriptors.property import Lazy
-
-from z3c.rml import rml2pdf
-
-from nti.app.analytics_pandas.views.assessments import AssessmentsEventsTimeseriesReportView
-from nti.app.analytics_pandas.views.assessments import AssessmentsEventsTimeseriesContext
-
-from nti.app.analytics_pandas.tests import AppAnalyticsTestBase
 from nti.app.analytics_pandas.tests import PandasReportsLayerTest
 
-from nti.app.analytics_pandas.views.tests import _build_sample_context
-
-from nti.app.pyramid_zope.z3c_zpt import ViewPageTemplateFile
+from nti.app.testing.application_webtest import ApplicationLayerTest
 
 from nti.app.testing.decorators import WithSharedApplicationMockDS
 
-from nti.externalization.externalization import to_external_object
 
-class TestAssessmentsEvents(AppAnalyticsTestBase):
+class TestAssessmentViews(ApplicationLayerTest):
 
-	def setUp(self):
-		super(TestAssessmentsEvents, self).setUp()
+    layer = PandasReportsLayerTest
 
-	@Lazy
-	def std_report_layout_rml(self):
-		path = os.path.join(
-                    os.path.dirname(__file__),
-                    '../../templates/std_report_layout.rml')
-		return path
-
-	def template(self, path):
-		result = ViewPageTemplateFile(path,
-                                auto_reload=(False,),
-                                debug=False)
-		return result
-
-	def test_std_report_layout_rml(self):
-		# make sure  template exists
-		path = self.std_report_layout_rml
-		assert_that(os.path.exists(path), is_(True))
-
-		# prepare view and context
-		context = AssessmentsEventsTimeseriesContext()
-		view = AssessmentsEventsTimeseriesReportView(context)
-		view._build_data('Bleach')
-		system = {'view': view, 'context': context}
-		rml = self.template(path).bind(view)(**system)
-
-		pdf_stream = rml2pdf.parseString(rml)
-		result = pdf_stream.read()
-		assert_that(result, has_length(greater_than(1)))
-
-
-class TestAssessmentViews(PandasReportsLayerTest):
-
-	@WithSharedApplicationMockDS(testapp=True, users=True)
-	def test_assessment_view(self):
-		context = _build_sample_context(AssessmentsEventsTimeseriesContext)
-		params = to_external_object(context)
-		response = self.testapp.post('/dataserver2/pandas_reports/AssessmentsRelatedEvents',
-									json.dumps(params),
+    @WithSharedApplicationMockDS(testapp=True, users=True)
+    def test_assessment_report(self):
+        response = self.testapp.get('/dataserver2/pandas_reports/AssessmentsReport',
                                     extra_environ=self._make_extra_environ())
-		assert_that(response, has_property('content_type', 'application/pdf'))
+        assert_that(response,
+                    has_property('content_type', 'application/pdf'))
