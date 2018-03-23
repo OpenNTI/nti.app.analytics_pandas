9155e0cc5b484dd84771538e6d728ea951a279eb
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-03-23 11:01:39 -0500

    WIP for ega

--- src/nti/app/analytics_pandas/views/tests/test_assessments.py
+++ src/nti/app/analytics_pandas/views/tests/test_assessments.py
@@ -7,10 +7,16 @@ from __future__ import absolute_import
 
 # pylint: disable=protected-access,too-many-public-methods
 
+from hamcrest import none
+from hamcrest import is_not
 from hamcrest import assert_that
 from hamcrest import has_property
 
+from pyramid.testing import DummyRequest
+
 from nti.app.analytics_pandas.tests import PandasReportsLayerTest
+
+from nti.app.analytics_pandas.views.assessments import AssessmentsTimeseriesReportView
 
 from nti.app.testing.application_webtest import ApplicationLayerTest
 
@@ -24,9 +30,21 @@ class TestAssessmentViews(ApplicationLayerTest):
     @WithSharedApplicationMockDS(testapp=True, users=True)
     def test_assessment_report(self):
         response = self.testapp.post_json('/dataserver2/pandas_reports/AssessmentsReport',
-										  {
-											 'ntiid': 'course-ntiid',
-										  },
-                                    	  extra_environ=self._make_extra_environ())
+                                          {
+                                              'ntiid': 'course-ntiid',
+                                          },
+                                          extra_environ=self._make_extra_environ())
         assert_that(response,
                     has_property('content_type', 'application/pdf'))
+
+
+class TestAssessmentOptions(ApplicationLayerTest):
+
+    layer = PandasReportsLayerTest
+
+    @WithSharedApplicationMockDS(testapp=True, users=True)
+    def test_assessment_report(self):
+        request = DummyRequest(post={'ntiid': 'course-ntiid', })
+        view = AssessmentsTimeseriesReportView(request)
+        options = view()
+        assert_that(options, is_not(none()))
