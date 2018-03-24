5b73effc2a89111fed7799f76d90f7d79e5c96d5
Egawati Panjei <egawati.panjei@gmail.com>
2018-03-24 09:50:17 +0700

    test videos

--- src/nti/app/analytics_pandas/views/tests/test_videos.py
+++ src/nti/app/analytics_pandas/views/tests/test_videos.py
@@ -1,79 +1,84 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import print_function, unicode_literals, absolute_import, division
-__docformat__ = "restructuredtext en"
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
 
-# disable: accessing protected members, too many methods
-# pylint: disable=W0212,R0904
+# pylint: disable=protected-access,too-many-public-methods
 
 from hamcrest import is_
-from hamcrest import has_length
+from hamcrest import none
+from hamcrest import is_not
 from hamcrest import assert_that
-from hamcrest import greater_than
+from hamcrest import has_entries
 from hamcrest import has_property
 
-import os
-import json
+from pyramid.testing import DummyRequest
 
-from zope.cachedescriptors.property import Lazy
-
-from z3c.rml import rml2pdf
-
-from nti.app.analytics_pandas.views.videos import VideosTimeseriesReportView
-from nti.app.analytics_pandas.views.videos import VideosTimeseriesContext
-
-from nti.app.analytics_pandas.tests import AppAnalyticsTestBase
 from nti.app.analytics_pandas.tests import PandasReportsLayerTest
 
-from nti.app.analytics_pandas.views.tests import _build_sample_context
+from nti.app.analytics_pandas.views.videos import VideosTimeseriesReportView
 
-from nti.app.pyramid_zope.z3c_zpt import ViewPageTemplateFile
+from nti.app.testing.application_webtest import ApplicationLayerTest
 
 from nti.app.testing.decorators import WithSharedApplicationMockDS
 
-from nti.externalization.externalization import to_external_object
+
+class TestVideoView(ApplicationLayerTest):
+
+    layer = PandasReportsLayerTest
+
+    @WithSharedApplicationMockDS(testapp=True, users=True)
+    def test_video_report(self):
+        response = self.testapp.post_json('/dataserver2/pandas_reports/HighlightsReport',
+                                          {
+                                              'ntiid': 'tag:nextthought.com,2011-10:NTI-CourseInfo-Spring2015_SOC_1113',
+                                              'start_date': '2015-02-01',
+                                              'end_date': '2015-02-28'
+                                          },
+                                          extra_environ=self._make_extra_environ())
+        assert_that(response,
+                    has_property('content_type', 'application/pdf'))
 
 
-class TestVideosEvents(AppAnalyticsTestBase):
+class TestVideoOptions(ApplicationLayerTest):
 
-	@Lazy
-	def std_report_layout_rml(self):
-		path = os.path.join(
-                    os.path.dirname(__file__),
-                    '../../templates/std_report_layout.rml')
-		return path
+    layer = PandasReportsLayerTest
 
-	def template(self, path):
-		result = ViewPageTemplateFile(path,
-                                auto_reload=(False,),
-                                debug=False)
-		return result
-
-	def test_std_report_layout_rml(self):
-		# make sure  template exists
-		path = self.std_report_layout_rml
-		assert_that(os.path.exists(path), is_(True))
-
-		# prepare view and context
-		context = VideosTimeseriesContext()
-		view = VideosTimeseriesReportView(context)
-		view._build_data('Bleach')
-		system = {'view': view, 'context': context}
-		rml = self.template(path).bind(view)(**system)
-
-		pdf_stream = rml2pdf.parseString(rml)
-		result = pdf_stream.read()
-		assert_that(result, has_length(greater_than(1)))
-
-
-class TestVideosViews(PandasReportsLayerTest):
-
-	@WithSharedApplicationMockDS(testapp=True, users=True)
-	def test_video_view(self):
-		context = _build_sample_context(VideosTimeseriesContext)
-		params = to_external_object(context)
-		response = self.testapp.post('/dataserver2/pandas_reports/VideosRelatedEvents',
-                               json.dumps(params),
-                               extra_environ=self._make_extra_environ())
-		assert_that(response, has_property('content_type', 'application/pdf'))
+    @WithSharedApplicationMockDS(testapp=True, users=True)
+    def test_video__report(self):
+        request = DummyRequest(params={'ntiid': 'tag:nextthought.com,2011-10:NTI-CourseInfo-Spring2015_SOC_1113',
+                                       'start_date': '2015-02-01',
+                                       'end_date': '2015-02-28'})
+        view = VideosTimeseriesReportView(request=request)
+        options = view()
+        assert_that(options, is_not(none()))
+        assert_that(options,
+                    has_entries('course_ids', is_([388]),
+                                'has_video_events_data', True,
+                                'has_videos_watched', True,
+                                'has_videos_skipped', True,
+                                'has_videos_watched_per_enrollment_types', True,
+                                'has_videos_skipped_per_enrollment_types', True))
+        assert_that(options,
+                    has_entries('data',
+                                has_entries('video_events', is_not(none()),
+                                            'video_events', 
+                                            has_entries('num_rows_video_event_type', 35,
+                                                        'by_video_event_type_chart', is_not(none()),
+                                                        'tuples_video_event_type', (),
+                                                        'num_rows', 25,
+                                                        'events_chart', is_not(none()),
+                                                        'tuples', (),
+                                                        'column_name', u'Videos Watched',
+                                                        'num_rows_skip', 10,
+                                                        'column_name_skip', u'Videos Skipped',
+                                                        'events_chart_skip', is_not(none()),
+                                                        'tuples_skip', (),
+                                                        'videos_watched', is_not(none()),
+                                                        'videos_skipped', is_not(none())
+                                                        )
+                                            )
+                                )
+                    )
