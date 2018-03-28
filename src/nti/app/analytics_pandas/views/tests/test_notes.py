b661a125e3cfb4624ad987259a7fabf8a78d1bf4
Egawati Panjei <egawati.panjei@gmail.com>
2018-03-28 10:58:44 +0700

    add test on notes report: one day only

--- src/nti/app/analytics_pandas/views/tests/test_notes.py
+++ src/nti/app/analytics_pandas/views/tests/test_notes.py
@@ -77,3 +77,35 @@ class TestNotesOptions(ApplicationLayerTest):
                                             )
                                 )
                     )
+
+    @WithSharedApplicationMockDS(testapp=True, users=True)
+    def test_notes_report_one_day(self):
+        request = DummyRequest(params={'ntiid': 'tag:nextthought.com,2011-10:NTI-CourseInfo-Spring2015_SOC_1113',
+                                       'start_date': '2015-01-15',
+                                       'end_date': '2015-01-15'})
+        view = NotesTimeseriesReportView(request=request)
+        options = view()
+        assert_that(options, is_not(none()))
+        assert_that(options,
+                    has_entries('course_ids', is_([388]),
+                                'has_note_view_events', True,
+                                'has_note_create_events', True,
+                                'has_note_like_events', False,
+                                'has_note_favorite_events', False))
+        assert_that(options,
+                    has_entries('data',
+                                has_entries('notes_viewed', is_not(none()),
+                                            'notes_viewed', 
+                                            has_entries('num_rows', 1,
+                                                        'events_chart', (),
+                                                        'tuples', is_not(none()),
+                                                        'column_name', u'Notes Viewed'),
+                                            'notes_created', is_not(none()),
+                                            'notes_created', 
+                                            has_entries('num_rows', 1,
+                                                        'events_chart', (),
+                                                        'tuples', is_not(none()),
+                                                        'column_name', u'Notes Created')
+                                            )
+                                )
+                    )
