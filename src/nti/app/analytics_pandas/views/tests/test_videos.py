83aa0922c9d479ca94199aba70b347d1f52adca5
Egawati Panjei <egawati.panjei@gmail.com>
2018-03-28 11:15:07 +0700

    add test on videos report: one day only

--- src/nti/app/analytics_pandas/views/tests/test_videos.py
+++ src/nti/app/analytics_pandas/views/tests/test_videos.py
@@ -47,7 +47,7 @@ class TestVideoOptions(ApplicationLayerTest):
     layer = PandasReportsLayerTest
 
     @WithSharedApplicationMockDS(testapp=True, users=True)
-    def test_video__report(self):
+    def test_video_report(self):
         request = DummyRequest(params={'ntiid': 'tag:nextthought.com,2011-10:NTI-CourseInfo-Spring2015_SOC_1113',
                                        'start_date': '2015-02-01',
                                        'end_date': '2015-02-28'})
@@ -76,6 +76,43 @@ class TestVideoOptions(ApplicationLayerTest):
                                                         'column_name_skip', u'Videos Skipped',
                                                         'events_chart_skip', is_not(none()),
                                                         'tuples_skip', (),
+                                                        'videos_watched', is_not(none()),
+                                                        'videos_skipped', is_not(none())
+                                                        )
+                                            )
+                                )
+                    )
+
+    @WithSharedApplicationMockDS(testapp=True, users=True)
+    def test_video_report_one_day(self):
+        request = DummyRequest(params={'ntiid': 'tag:nextthought.com,2011-10:NTI-CourseInfo-Spring2015_SOC_1113',
+                                       'start_date': '2015-02-28',
+                                       'end_date': '2015-02-28'})
+        view = VideosTimeseriesReportView(request=request)
+        options = view()
+        assert_that(options, is_not(none()))
+        assert_that(options,
+                    has_entries('course_ids', is_([388]),
+                                'has_video_events_data', True,
+                                'has_videos_watched', True,
+                                'has_videos_skipped', True,
+                                'has_videos_watched_per_enrollment_types', True,
+                                'has_videos_skipped_per_enrollment_types', True))
+        assert_that(options,
+                    has_entries('data',
+                                has_entries('video_events', is_not(none()),
+                                            'video_events', 
+                                            has_entries('num_rows_video_event_type', 2,
+                                                        'by_video_event_type_chart', (),
+                                                        'tuples_video_event_type', is_not(none()),
+                                                        'num_rows', 1,
+                                                        'events_chart', (),
+                                                        'tuples', is_not(none()),
+                                                        'column_name', u'Videos Watched',
+                                                        'num_rows_skip', 1,
+                                                        'column_name_skip', u'Videos Skipped',
+                                                        'events_chart_skip', (),
+                                                        'tuples_skip', is_not(none()),
                                                         'videos_watched', is_not(none()),
                                                         'videos_skipped', is_not(none())
                                                         )
